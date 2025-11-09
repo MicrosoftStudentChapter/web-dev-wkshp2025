194443d2d15b793de648c183ef16d87bb6187e6b
Rishit Garg <rishitalig@gmail.com>
2025-11-09 22:26:17 +0530

    Delete

--- submissions/Rishit-grg/script.js
+++
@@ -1,9 +0,0 @@
-// Script for smooth scrolling between sections
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
